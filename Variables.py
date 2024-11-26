4d21794aa547332f192acb69a7b59002429bd35d
Salim Bello <saleemyoungarmy3@gmail.com>
2024-11-26 16:09:42 +0100

    Python Programming Excersise Day Two

---
+++ Variables.py
@@ -0,0 +1,114 @@
+'''
+Day 2: Python Programming Exercise
+
+'''
+
+#Exercise Level 1
+
+first_name = "Salim"
+last_name = "Bello"
+
+full_name = first_name + "" + last_name
+
+country = "Nigeria"
+
+city = "Nasarawa"
+
+age = 24
+
+year = 2024
+
+is_married = False
+
+is_true = True
+
+is_light_on = True
+
+x, y, z = 10, "ahmad", "adamu"
+
+#Exercise Level 2
+
+#1
+print(type(first_name))
+
+print(type(last_name))
+
+print(type(full_name))
+
+print(type(country))
+
+print(type(city))
+
+print(type(age))
+
+print(type(year))
+
+print(type(is_married))
+
+print(type(is_true))
+
+print(type(is_light_on))
+
+#2
+
+print(len(first_name))
+
+#3
+
+print(len(first_name))
+print(len(last_name))
+print(len(first_name)==len(last_name))
+
+#4
+
+num_one = 5
+num_two = 4
+
+#5
+
+total = num_one + num_two
+
+#6
+
+diff = num_one - num_two
+
+#7
+
+product = num_one * num_two
+
+#8
+
+division = num_one / num_two
+
+#9
+
+remainder = num_two % num_one
+
+#10
+
+exp = num_one ** num_two
+
+#11 
+
+floor = num_one // num_two
+
+#12
+
+area_of_circle = 3.142 * (30 ** 2)
+
+circum_of_circle = 2 * 3.142 * 30
+
+radius = int(input("Please Enter A radius "))
+
+area_of_circle = 3.142 * radius * radius
+
+#13
+
+first_name = input("Enter First Name: ")
+last_name = input("Enter Last Name: ")
+country = input("Enter Country Name: ")
+age = input("Enter Age:" )
+
+#14
+
+print(help("keywords"))
